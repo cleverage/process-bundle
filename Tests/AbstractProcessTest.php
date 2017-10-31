2c5ac575493427b202d63edcc35bc672c1f7d1fb
Valentin Clavreul <vclavreul@clever-age.com>
2017-10-31 10:38:26 +0100

    Added comments

--- Tests/AbstractProcessTest.php
+++ Tests/AbstractProcessTest.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ *    CleverAge/ProcessBundle
+ *    Copyright (C) 2017 Clever-Age
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace CleverAge\ProcessBundle\Tests;
 
@@ -7,6 +24,9 @@ use CleverAge\ProcessBundle\Manager\ProcessManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * Provide all necessary setup to test a process
+ */
 abstract class AbstractProcessTest extends KernelTestCase
 {
     /** @var  ContainerInterface */
