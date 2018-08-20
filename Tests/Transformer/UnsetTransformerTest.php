d30f3db0f5a670da02b595798067636aedb129c1
Valentin C <valentin@clavreul.com>
2018-08-20 15:40:12 +0200

    Added some more tests

--- Tests/Transformer/UnsetTransformerTest.php
+++ Tests/Transformer/UnsetTransformerTest.php
@@ -18,9 +18,60 @@ class UnsetTransformerTest extends AbstractProcessTest
         $input = [
             'other'    => 1,
             'to_unset' => 1,
+            'to_test'  => 2,
         ];
         $result = $this->processManager->execute('test.unset_transformer.simple', null, $input);
-        self::assertEquals(['other' => 1], $result);
+        self::assertEquals(['other' => 1, 'to_test' => 2], $result);
+    }
+
+    /**
+     * Assert a few simple condition can trigger unset (or not)
+     */
+    public function testConditionalUnset()
+    {
+        $input = [
+            'other'    => 1,
+            'to_unset' => 1,
+            'to_test'  => 2,
+        ];
+
+        // Should unset
+        $result = $this->processManager->execute('test.unset_transformer.condition', null, $input);
+        self::assertEquals(['other' => 1, 'to_test' => 2], $result);
+
+        // No unset
+        $input['to_test'] = 3;
+        $result = $this->processManager->execute('test.unset_transformer.condition', null, $input);
+        self::assertEquals(['other' => 1, 'to_unset' => 1, 'to_test' => 3], $result);
+
+        // Checking null, no unset
+        $result = $this->processManager->execute('test.unset_transformer.condition_null', null, $input);
+        self::assertEquals(['other' => 1, 'to_unset' => 1, 'to_test' => 3], $result);
+
+        // Should unset
+        $input['to_test'] = null;
+        $result = $this->processManager->execute('test.unset_transformer.condition_null', null, $input);
+        self::assertEquals(['other' => 1, 'to_test' => null], $result);
+    }
+
+    /**
+     * Assert the transformer detect wrong types
+     *
+     * @expectedException \RuntimeException
+     */
+    public function testWrongUnsetString()
+    {
+        $this->processManager->execute('test.unset_transformer.simple', null, 'not an array');
+    }
+
+    /**
+     * Assert the transformer detect wrong values
+     *
+     * @expectedException \RuntimeException
+     */
+    public function testWrongUnsetMissingProperty()
+    {
+        $this->processManager->execute('test.unset_transformer.simple', null, ['no property found']);
     }
 
 }
