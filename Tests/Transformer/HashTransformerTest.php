d02e0d10a8fd2d8b95d866fda7561843589b724a
Madeline VEYRENC <mveyrenc@clever-age.com>
2019-01-23 10:11:00 +0100

    Add hash transformer

---
+++ Tests/Transformer/HashTransformerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CleverAge\ProcessBundle\Tests\Transformer;
+
+
+use CleverAge\ProcessBundle\Tests\AbstractProcessTest;
+
+/**
+ * Tests for Hash transformer
+ */
+class HashTransformerTest extends AbstractProcessTest
+{
+
+    /**
+     * Assert a string can be hash in md5
+     */
+    public function testMd5Hash()
+    {
+        $result = $this->processManager->execute('test.hash_transformer.md5', 'This is a string');
+        self::assertEquals('41fb5b5ae4d57c5ee528adb00e5e8e74', $result);
+    }
+
+    /**
+     * Assert a string can be hash in sha512
+     */
+    public function testSha512Hash()
+    {
+        $result = $this->processManager->execute('test.hash_transformer.sha512', 'This is a string');
+        self::assertEquals('f4d54d32e3523357ff023903eaba2721e8c8cfc7702663782cb3e52faf2c56c002cc3096b5f2b6df870be665d0040e9963590eb02d03d166e52999cd1c430db1', $result);
+    }
+}
---
+++ Transformer/HashTransformer.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
+ *
+ * Copyright (C) 2017-2018 Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CleverAge\ProcessBundle\Transformer;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Use hash() function to generate hash value
+ *
+ * @author Madeline Veyrenc <mveyrenc@clever-age.com>
+ */
+class HashTransformer implements ConfigurableTransformerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired('algo');
+        $resolver->setAllowedValues('algo', hash_algos());
+        $resolver->setAllowedTypes('algo', 'string');
+
+        $resolver->setDefined('raw_output');
+        $resolver->setDefault('raw_output', false);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws \UnexpectedValueException
+     */
+    public function transform($value, array $options = [])
+    {
+        return hash($options['algo'], $value, $options['raw_output']);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCode()
+    {
+        return 'hash';
+    }
+}
