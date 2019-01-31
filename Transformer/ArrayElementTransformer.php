0a2363d59754f8344fb8636d5d847947a7a9c731
Vincent Chalnot <vchalnot@clever-age.com>
2019-01-31 18:23:53 +0100

    New transformers: array_last and array_element

---
+++ Transformer/ArrayElementTransformer.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
+ *
+ * Copyright (C) 2017-2018 Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CleverAge\ProcessBundle\Transformer;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Return the nth element of an array
+ *
+ * @author Vincent Chalnot <vchalnot@clever-age.com>
+ */
+class ArrayElementTransformer implements ConfigurableTransformerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value, array $options = [])
+    {
+        return array_values(array_slice($value, $options['index'], 1))[0];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        return 'array_element';
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     *
+     * @throws \Symfony\Component\OptionsResolver\Exception\ExceptionInterface
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(
+            [
+                'index',
+            ]
+        );
+        $resolver->setAllowedTypes('index', ['integer']);
+    }
+}
---
+++ Transformer/ArrayLastTransformer.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
+ *
+ * Copyright (C) 2017-2018 Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CleverAge\ProcessBundle\Transformer;
+
+/**
+ * Return the last element of an array
+ *
+ * @author Vincent Chalnot <vchalnot@clever-age.com>
+ */
+class ArrayLastTransformer implements TransformerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value, array $options = [])
+    {
+        return array_values(array_slice($value, -1))[0];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        return 'array_last';
+    }
+}
