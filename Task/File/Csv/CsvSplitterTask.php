da257e0f673efe9716fd32a5195c2f2ce90a87b4
Valentin Clavreul <vclavreul@clever-age.com>
2019-04-01 18:12:27 +0200

    Removed suboptimal log for big files (due to "getLineCount" parsing the whole file)

--- Task/File/Csv/CsvSplitterTask.php
+++ Task/File/Csv/CsvSplitterTask.php
@@ -43,9 +43,6 @@ class CsvSplitterTask extends InputCsvReaderTask
                 $options['mode']
             );
 
-            if ($csv->getLineCount() > $options['max_lines']) {
-                $this->logger->debug("Found big CSV file ({$csv->getLineCount()} lines), splitting...");
-            }
             $this->csv = $csv;
         }
 
