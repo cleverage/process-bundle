174f09f054b4f1e2ec6ba04b7702ca2bb1073f46
Valentin Clavreul <vclavreul@clever-age.com>
2019-05-10 14:56:39 +0200

    Added tests & documentation for the rules transformer

---
+++ Tests/Transformer/RulesTransformerTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
+ *
+ * Copyright (C) 2017-2019 Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CleverAge\ProcessBundle\Tests\Transformer;
+
+use CleverAge\ProcessBundle\Tests\AbstractProcessTest;
+
+class RulesTransformerTest extends AbstractProcessTest
+{
+
+    /**
+     * Assert basic rules types
+     *
+     * @throws \Exception
+     */
+    public function testSimpleRule()
+    {
+        $result1 = $this->processManager->execute('test.rules_transformer.simple', 'ok');
+        self::assertEquals('result1', $result1);
+
+        $result2 = $this->processManager->execute('test.rules_transformer.simple', 'ko');
+        self::assertEquals('result2', $result2);
+
+        $result3 = $this->processManager->execute('test.rules_transformer.simple', 'any');
+        self::assertEquals('result3', $result3);
+    }
+
+}
