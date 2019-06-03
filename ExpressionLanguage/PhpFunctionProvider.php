1b5870a1c8078389f4131a1072913fa0516c9f08
Valentin Clavreul <vclavreul@clever-age.com>
2019-06-03 18:16:16 +0200

    Added a provider for ExpressionLanguage to override available functions

---
+++ ExpressionLanguage/PhpFunctionProvider.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
+ *
+ * Copyright (C) 2017-2019 Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CleverAge\ProcessBundle\ExpressionLanguage;
+
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
+
+/**
+ * Allow to inject a set of PHP function into an ExpressionLanguage instance
+ *
+ * @author Valentin Clavreul <vclavreul@clever-age.com>
+ */
+class PhpFunctionProvider implements ExpressionFunctionProviderInterface
+{
+    /** @var array */
+    protected $functions;
+
+    /**
+     * PhpFunctionProvider constructor.
+     *
+     * @param array $functions
+     */
+    public function __construct(array $functions)
+    {
+        $this->functions = $functions;
+    }
+
+    /**
+     * @return ExpressionFunction[]
+     */
+    public function getFunctions()
+    {
+        return array_map(function ($func) {
+            return ExpressionFunction::fromPhp($func);
+        }, $this->functions);
+    }
+}
