cb5f80b60598c39aee572663b2518b572d1eab41
Valentin Clavreul <vclavreul@clever-age.com>
2019-10-15 17:03:42 +0200

    Added a docker image to help with environment testing

---
+++ Resources/tests/environment/README.md
@@ -0,0 +1,4 @@
+Test environment
+================
+
+Those files are used to build a test environment for this bundle. For now, only Symfony4.3 is available.
---
+++ Resources/tests/environment/sf4/config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    CleverAge\ProcessBundle\CleverAgeProcessBundle::class => ['all' => true],
+    Sidus\BaseBundle\SidusBaseBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+];
