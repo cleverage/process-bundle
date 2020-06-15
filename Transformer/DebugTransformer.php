ae2ad46bf96288ca4739c08ff277276ec2738d04
Vincent Chalnot <vincent.chalnot@gmail.com>
2020-06-15 15:08:27 +0200

    Adding basic debug transformer

---
+++ Transformer/DebugTransformer.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
+ *
+ * Copyright (C) 2017-2019 Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CleverAge\ProcessBundle\Transformer;
+
+use Symfony\Component\VarDumper\VarDumper;
+
+/**
+ * Simple dump in a transformer, passthrough for value
+ *
+ * @author Vincent Chalnot <vchalnot@clever-age.com>
+ */
+class DebugTransformer implements TransformerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function transform($value, array $options = [])
+    {
+        if (class_exists(VarDumper::class)) {
+            VarDumper::dump($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCode()
+    {
+        return 'dump';
+    }
+}
