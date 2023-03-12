1626c38f0fad56a596299d527f384181247dc72a
Xavier Marchegay <xavier.marchegay@gmail.com>
2023-03-12 10:15:28 +0100

    upgrade to php8.1 and symfony 6

--- Resources/tests/environment/sf3/config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    CleverAge\ProcessBundle\CleverAgeProcessBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-];
