f5cfa247028100467a1253a6eefa2250333f603c
Xavier Marchegay <xavier.marchegay@gmail.com>
2023-03-12 10:39:18 +0100

    add rector

---
+++ rector.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/Command',
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Context',
+        __DIR__ . '/DependencyInjection',
+        __DIR__ . '/Event',
+        __DIR__ . '/EventDispatcher',
+        __DIR__ . '/EventListener',
+        __DIR__ . '/Exception',
+        __DIR__ . '/ExpressionLanguage',
+        __DIR__ . '/Filesystem',
+        __DIR__ . '/Logger',
+        __DIR__ . '/Manager',
+        __DIR__ . '/Model',
+        __DIR__ . '/Registry',
+        __DIR__ . '/Resources',
+        __DIR__ . '/Task',
+        __DIR__ . '/Tests',
+        __DIR__ . '/Transformer',
+        __DIR__ . '/Validator',
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SymfonyLevelSetList::UP_TO_SYMFONY_54
+    ]);
+};
