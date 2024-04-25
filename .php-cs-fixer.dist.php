42a7293ac8c8faa8e8c436ab770212e94ebf036b
Xavier Marchegay <xavier.marchegay@gmail.com>
2024-04-25 20:17:43 +0200

    update copyright

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the CleverAge/ProcessBundle package.
  *
- * Copyright (c) 2017-2023 Clever-Age
+ * Copyright (c) 2017-2024 Clever-Age
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@ if (!file_exists(__DIR__.'/src')) {
 $fileHeaderComment = <<<'EOF'
 This file is part of the CleverAge/ProcessBundle package.
 
-Copyright (c) 2017-2023 Clever-Age
+Copyright (c) 2017-2024 Clever-Age
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
