4a6b58d5e1f1362da14201ecda363bc384592e55
Xavier Marchegay <xavier.marchegay@gmail.com>
2024-04-25 20:10:42 +0200

    update copyright

--- src/Task/Debug/DieTask.php
+++ src/Task/Debug/DieTask.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the CleverAge/ProcessBundle package.
  *
- * Copyright (c) 2017-2023 Clever-Age
+ * Copyright (c) 2017-2024 Clever-Age
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,6 +15,7 @@ namespace CleverAge\ProcessBundle\Task\Debug;
 
 use CleverAge\ProcessBundle\Model\ProcessState;
 use CleverAge\ProcessBundle\Model\TaskInterface;
+use Symfony\Component\Console\Helper\Helper;
 
 /**
  * Class DieTask.
@@ -27,6 +28,7 @@ class DieTask implements TaskInterface
 {
     public function execute(ProcessState $state): never
     {
+        var_dump(Helper::formatMemory(memory_get_peak_usage(true)));
         exit;
     }
 }
