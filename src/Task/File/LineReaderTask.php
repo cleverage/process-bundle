55d4667532b227ec4b6e3cfda911fd3d6e9ed993
Nicolas Joubert <njoubert@clever-age.com>
2025-01-02 15:58:16 +0100

    #155 php-cs-fixer

--- src/Task/File/LineReaderTask.php
+++ src/Task/File/LineReaderTask.php
@@ -2,9 +2,17 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
+ *
+ * Copyright (c) Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CleverAge\ProcessBundle\Task\File;
 
-use CleverAge\ProcessBundle\Filesystem\CsvFile;
 use CleverAge\ProcessBundle\Model\AbstractConfigurableTask;
 use CleverAge\ProcessBundle\Model\IterableTaskInterface;
 use CleverAge\ProcessBundle\Model\ProcessState;
